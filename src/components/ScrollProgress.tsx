940d53541492fdf46d47a31a63eb4ece075be412
minseokKim6823 <alstjr6823@naver.com>
2026-04-01 00:28:50 +0000

    Add scroll progress bar
    Co-authored-by: minseokKim6823 <93479041+minseokKim6823@users.noreply.github.com>

--- src/components/ScrollProgress.tsx
+++ src/components/ScrollProgress.tsx
@@ -1,7 +1,8 @@
 import { motion, useScroll, useSpring } from "framer-motion";
+import { RefObject } from "react";
 
-const ScrollProgress = () => {
-  const { scrollYProgress } = useScroll();
+const ScrollProgress = ({ containerRef }: { containerRef: RefObject<HTMLDivElement> }) => {
+  const { scrollYProgress } = useScroll({ container: containerRef });
   const scaleX = useSpring(scrollYProgress, { stiffness: 100, damping: 30, restDelta: 0.001 });
 
   return (
